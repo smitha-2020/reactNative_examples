60fcb65c83821f2bd04070384df56beb787a69b8
Salusfin <salusfin@Salusfins-MacBook-Pro.local>
2025-06-30 12:23:24 +0300

    example1

--- App.tsx
+++ App.tsx
@@ -25,6 +25,29 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <View style={{ marginBottom: 10 }}>
+            <TextInput
+              label="Enter Goal"
+              value={goal}
+              mode="outlined"
+              onChangeText={goal => setGoal(goal)}
+              multiline={false}
+              numberOfLines={1}
+            />
+          </View>
+          <Button mode="contained" onPress={() => addGoalFn()}>
+            Add Goal
+          </Button>
+        </View>
+
+        <FlatListView
+          goals={goals}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      </View>
     </View>
   );
 }
